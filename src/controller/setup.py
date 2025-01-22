8fab9b2ba4b71214ef7f5701fdb4c1914e0c15f6
khushant2001 <khushant.khurana11@gmail.com>
2025-01-21 22:34:39 -0500

    Preparing for kalman filter development

--- src/controller/setup.py
+++ src/controller/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'controller = controller.controller:main'
+            'controller = controller.controller:main',
+            'kalman_filter = controller.kalman_filter:main'
         ],
     },
 )
